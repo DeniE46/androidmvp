2278dfdff0eab323703bc3833940135ba512b2f3
Denislav Popov <denislavpopov46@gmail.com>
2017-11-05 00:38:51 +0200

    Initial commit, added DataBinding library

--- app/src/main/java/com/antonioleiva/mvpexample/app/Login/LoginActivity.java
+++ app/src/main/java/com/antonioleiva/mvpexample/app/Login/LoginActivity.java
@@ -20,31 +20,30 @@ package com.antonioleiva.mvpexample.app.Login;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.ProgressBar;
 
 import com.antonioleiva.mvpexample.app.R;
+import com.antonioleiva.mvpexample.app.databinding.ActivityLoginBinding;
 import com.antonioleiva.mvpexample.app.main.MainActivity;
 
 public class LoginActivity extends Activity implements LoginView, View.OnClickListener {
 
-    private ProgressBar progressBar;
-    private EditText username;
-    private EditText password;
+    ActivityLoginBinding activityLoginBinding;
+
+
     private LoginPresenter presenter;
 
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
-        progressBar = (ProgressBar) findViewById(R.id.progress);
-        username = (EditText) findViewById(R.id.username);
-        password = (EditText) findViewById(R.id.password);
-        findViewById(R.id.button).setOnClickListener(this);
-
+        activityLoginBinding = DataBindingUtil.setContentView(this, R.layout.activity_login);
+        initializeViews();
         presenter = new LoginPresenterImpl(this);
+    }
+
+    void initializeViews(){
+        findViewById(R.id.button).setOnClickListener(this);
     }
 
     @Override protected void onDestroy() {
@@ -53,19 +52,19 @@ public class LoginActivity extends Activity implements LoginView, View.OnClickLi
     }
 
     @Override public void showProgress() {
-        progressBar.setVisibility(View.VISIBLE);
+        activityLoginBinding.progress.setVisibility(View.VISIBLE);
     }
 
     @Override public void hideProgress() {
-        progressBar.setVisibility(View.GONE);
+        activityLoginBinding.progress.setVisibility(View.GONE);
     }
 
     @Override public void setUsernameError() {
-        username.setError(getString(R.string.username_error));
+        activityLoginBinding.username.setError(getString(R.string.username_error));
     }
 
     @Override public void setPasswordError() {
-        password.setError(getString(R.string.password_error));
+        activityLoginBinding.password.setError(getString(R.string.password_error));
     }
 
     @Override public void navigateToHome() {
@@ -74,6 +73,6 @@ public class LoginActivity extends Activity implements LoginView, View.OnClickLi
     }
 
     @Override public void onClick(View v) {
-        presenter.validateCredentials(username.getText().toString(), password.getText().toString());
+        presenter.validateCredentials(activityLoginBinding.username.getText().toString(), activityLoginBinding.password.getText().toString());
     }
 }
